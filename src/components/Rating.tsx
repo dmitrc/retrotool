7de29a70f185b3abe29a87a58bad02d2536cc28b
Dmitrii Cucleschin <dmitc@microsoft.com>
2019-02-08 11:29:57 -0800

    fix rating control's value

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -8,33 +8,28 @@ import * as MdThumbsDown from "react-ionicons/lib/MdThumbsDown";
 
 export const Rating = (props: RatingProps) => {
     const [rating, setRating] = useState(RatingStatus.NotSet);
-    const [value, setValue] = useState(props.value || 0);
+    const value = props.value || 0;
 
     const activeUp = rating == RatingStatus.Like;
     const activeDown = rating == RatingStatus.Dislike;
+    const valueDiff = activeUp ? 1 : (activeDown ? -1 : 0);
 
     const handleUp = () => {
         const newVal = activeUp ? RatingStatus.NotSet : RatingStatus.Like;
-        
         setRating(newVal);
-        setValue(prevValue => prevValue + 1);
-
         props.onChange && props.onChange(newVal);
     }
 
     const handleDown = () => {
         const newVal = activeDown ? RatingStatus.NotSet : RatingStatus.Dislike;
-
         setRating(newVal);
-        setValue(prevValue => prevValue - 1);
-
         props.onChange && props.onChange(newVal);
     }
 
     return (
         <div className="rating">
             <IconButton icon={MdThumbsDown} className={activeDown && "danger"} onClick={handleDown} />
-            <span>{value}</span>
+            <span>{value + valueDiff}</span>
             <IconButton icon={MdThumbsUp} className={activeUp && "success"} onClick={handleUp}  />
         </div>
     )
